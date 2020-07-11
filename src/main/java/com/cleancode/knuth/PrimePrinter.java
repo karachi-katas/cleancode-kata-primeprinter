f87851db69b788d68b738752e040b04f6f9992db
shakeeb <shakeebsiddiqui1998@gmail.com>
2020-07-11 17:18:14 +0500

    changed array into array List

--- src/main/java/com/cleancode/knuth/PrimePrinter.java
+++ src/main/java/com/cleancode/knuth/PrimePrinter.java
@@ -1,9 +1,17 @@
 package com.cleancode.knuth;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimePrinter {
 
     final int totalNumberOfPrimesToBePrinted = 1000;
-    int primes[] = new int[this.totalNumberOfPrimesToBePrinted +1];
+    List<Integer> primes = new ArrayList<Integer>(){
+        {
+            add(0);
+        }
+    };
+    int lastPerfectOddNumberSquareIndex = 2;
 
     public static void main(String[] args) {
 
@@ -15,63 +23,59 @@ public class PrimePrinter {
 
     private void calculatePrimes() {
 
-        final int ORDMAX = 30;
-        int multiplesOfOddNumbers[] = new int[ORDMAX+1];
+        final int maximumOddPerfectSquare = 30;
+        int multiplesOfOddNumbers[] = new int[maximumOddPerfectSquare + 1];
+//        primes.add(0);
+        primes.add(2);
 
-        primes[1] = 2;
-
-        int currentPrimeCount = 1;
         int currentNumberToCheck = 1;
         int currentSquare = 9;
-        int lastPerfectOddNumberSquareIndex = 2;
 
-        while (currentPrimeCount < this.totalNumberOfPrimesToBePrinted) {
+        while (primes.size() <= this.totalNumberOfPrimesToBePrinted) {
             boolean isCurrentNumberPrime;
 
             do {
                 currentNumberToCheck += 2;
-                if( currentNumberToCheck == currentSquare) {
+                if (currentNumberToCheck == currentSquare) {
                     lastPerfectOddNumberSquareIndex++;
-                    currentSquare=primes[lastPerfectOddNumberSquareIndex]*primes[lastPerfectOddNumberSquareIndex];
-                    multiplesOfOddNumbers[lastPerfectOddNumberSquareIndex-1]=currentNumberToCheck;
+                    currentSquare = getNextOddNumberSquare();
+                    multiplesOfOddNumbers[lastPerfectOddNumberSquareIndex - 1] = currentNumberToCheck;
                 }
                 int currentIndex = 2;
-                isCurrentNumberPrime=true;
+                isCurrentNumberPrime = true;
                 while (currentIndex < lastPerfectOddNumberSquareIndex && isCurrentNumberPrime) {
-                    while (multiplesOfOddNumbers[currentIndex]<currentNumberToCheck)
-                        multiplesOfOddNumbers[currentIndex] += primes[currentIndex] + primes[currentIndex];
+                    while (multiplesOfOddNumbers[currentIndex] < currentNumberToCheck)
+                        multiplesOfOddNumbers[currentIndex] += primes.get(currentIndex) + primes.get(currentIndex);
                     if (multiplesOfOddNumbers[currentIndex] == currentNumberToCheck)
-                        isCurrentNumberPrime=false;
+                        isCurrentNumberPrime = false;
                     currentIndex++;
                 }
             } while (!isCurrentNumberPrime);
-            currentPrimeCount++;
-            primes[currentPrimeCount]=currentNumberToCheck;
+            primes.add(currentNumberToCheck);
         }
     }
 
+    private int getNextOddNumberSquare() {
+        return primes.get(lastPerfectOddNumberSquareIndex) * primes.get(lastPerfectOddNumberSquareIndex);
+    }
+
     private void printPrimes() {
-        int currentPageNumber = 1;
-        int currentPageOffset = 1;
-        int currentRowOffset;
 
         final int totalRowsPerPage = 50;
         final int totalColumnsPerPage = 4;
+        int currentPage = 1;
 
-        int C;
-        while (currentPageOffset <= this.totalNumberOfPrimesToBePrinted) {
-            System.out.println(String.format("The First %d Prime Numbers === Page %d\n", this.totalNumberOfPrimesToBePrinted, currentPageNumber));
-            
-            for (currentRowOffset=currentPageOffset; currentRowOffset <= currentPageOffset+totalRowsPerPage-1; currentRowOffset++) {
-                for (C = 0; C <= totalColumnsPerPage - 1; C++)
-                    if (currentRowOffset + C * totalRowsPerPage <= this.totalNumberOfPrimesToBePrinted)
-                        System.out.printf("%10d", primes[currentRowOffset + C * totalRowsPerPage]);
+        for (int currentPageOffset = 1; currentPageOffset <= this.totalNumberOfPrimesToBePrinted; currentPageOffset += totalRowsPerPage * totalColumnsPerPage) {
+            System.out.println(String.format("The First %d Prime Numbers === Page %d\n", this.totalNumberOfPrimesToBePrinted, currentPage));
+
+            for (int currentRow = currentPageOffset; currentRow <= currentPageOffset + totalRowsPerPage - 1; currentRow++) {
+                for (int currentColumn = 0; currentColumn <= totalColumnsPerPage - 1; currentColumn++)
+                    if (currentRow + currentColumn * totalRowsPerPage <= this.totalNumberOfPrimesToBePrinted)
+                        System.out.printf("%10d", primes.get(currentRow + currentColumn * totalRowsPerPage));
                 System.out.println();
             }
             System.out.println("\f");
-            currentPageNumber++;
-            currentPageOffset += totalRowsPerPage*totalColumnsPerPage;
-
+            currentPage++;
         }
     }
 }
