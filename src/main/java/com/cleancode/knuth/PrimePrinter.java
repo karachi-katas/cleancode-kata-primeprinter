0477ca17e6f1a57aec64947aea0f6ac9cee16d04
Sauban bin Usman <saubanbinusman@yahoo.com>
2020-07-11 16:44:57 +0500

    Moved variables to their respective scopes, removed useless variables

--- src/main/java/com/cleancode/knuth/PrimePrinter.java
+++ src/main/java/com/cleancode/knuth/PrimePrinter.java
@@ -4,33 +4,30 @@ package com.cleancode.knuth;
 public class PrimePrinter {
     public static void main(String[] args) {
 		final int primeCountLimit = 1000;
-        final int RR = 50;
-        final int CC = 4;
-        final int ORDMAX = 40;
-        int P[] = new int[primeCountLimit+1];
 
-        int MULT[] = new int[ORDMAX+1];
-
-        loop1(primeCountLimit, P, MULT);
-
-        loop2(primeCountLimit, RR, CC, P);
+        int[] primes = generatePrimes(primeCountLimit);
+        printPrimes(primes);
     }
 
-    private static void loop2(int m, int RR, int CC, int[] p) {
+    private static void printPrimes(int[] primes) {
         int ROWOFFSET;
         int C;
         int PAGENUMBER = 1;
         int PAGEOFFSET = 1;
-        while (PAGEOFFSET <= m) {
+        int primeCount = primes.length - 1;
+        final int RR = 50;
+        final int CC = 4;
+
+        while (PAGEOFFSET <= primeCount) {
             System.out.print("The First ");
-            System.out.print(Integer.toString(m));
+            System.out.print(Integer.toString(primeCount));
             System.out.print(" Prime Numbers === Page ");
             System.out.print(Integer.toString(PAGENUMBER));
             System.out.println("\n");
             for (ROWOFFSET=PAGEOFFSET; ROWOFFSET <= PAGEOFFSET+RR-1; ROWOFFSET++) {
                 for (C = 0; C <= CC - 1; C++)
-                    if (ROWOFFSET + C * RR <= m)
-                        System.out.printf("%10d", p[ROWOFFSET + C * RR]);
+                    if (ROWOFFSET + C * RR <= primeCount)
+                        System.out.printf("%10d", primes[ROWOFFSET + C * RR]);
                 System.out.println();
             }
             System.out.println("\f");
@@ -40,9 +37,10 @@ public class PrimePrinter {
         }
     }
 
-    private static void loop1(int primeCountLimit, int[] primes, int[] MultiplesOfANumber) {
-        int N;
-        boolean JPRIME;
+    private static int[] generatePrimes(int primeCountLimit) {
+        int[] primes = new int[primeCountLimit + 1];
+        int ORDMAX = 30;
+        int[] multiplesOfANumber = new int[ORDMAX + 1];
         int numberCurrentlyBeingEvaluated=1;
         primes[1] = 2;
         int ORD = 2;
@@ -50,28 +48,35 @@ public class PrimePrinter {
 
         for (int primeIndex = 1; primeIndex < primeCountLimit; primeIndex++, primes[primeIndex] = numberCurrentlyBeingEvaluated) {
 
+            boolean isPrime;
+
+            // Increase numberCurrentlyBeingEvaluated until it is a prime number
             do {
                 numberCurrentlyBeingEvaluated += 2;
                 if( numberCurrentlyBeingEvaluated == SQUARE) {
                     ORD++;
-                    SQUARE= primes[ORD]* primes[ORD];
-                    MultiplesOfANumber[ORD-1]= numberCurrentlyBeingEvaluated;
-
+                    SQUARE = primes[ORD]* primes[ORD];
+                    multiplesOfANumber[ORD-1]= numberCurrentlyBeingEvaluated;
                 }
 
-                N=2;
-                JPRIME=true;
-                while (N < ORD && JPRIME) {
-                    while (MultiplesOfANumber[N] < numberCurrentlyBeingEvaluated) {
-                        MultiplesOfANumber[N] += primes[N] + primes[N];
-                    }
-                    if (MultiplesOfANumber[N] == numberCurrentlyBeingEvaluated)
-                        JPRIME=false;
-                    N++;
-                }
-            } while (!JPRIME);
-
+                isPrime = evaluatePrimality(primes, multiplesOfANumber, numberCurrentlyBeingEvaluated, ORD);
+            } while (!isPrime);
         }
 
+        return primes;
+    }
+
+    private static boolean evaluatePrimality(int[] primes, int[] multiplesOfANumber, int numberCurrentlyBeingEvaluated, int ORD) {
+        int N = 2;
+        boolean isPrime = true;
+        while (N < ORD && isPrime) {
+            while (multiplesOfANumber[N] < numberCurrentlyBeingEvaluated) {
+                multiplesOfANumber[N] += primes[N] + primes[N];
+            }
+            if (multiplesOfANumber[N] == numberCurrentlyBeingEvaluated)
+                isPrime=false;
+            N++;
+        }
+        return isPrime;
     }
 }
