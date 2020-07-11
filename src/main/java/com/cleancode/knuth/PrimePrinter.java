b92842eb116da83278fa226c1a05d9722941007f
zayantharani <zayantharani@gmail.com>
2020-07-11 15:48:06 +0500

    VariablesRenamed

--- src/main/java/com/cleancode/knuth/PrimePrinter.java
+++ src/main/java/com/cleancode/knuth/PrimePrinter.java
@@ -3,67 +3,68 @@ package com.cleancode.knuth;
 public class PrimePrinter {
     
     public static void main(String[] args) {
-		final int M = 1000;
-        final int RR = 50;
-        final int CC = 4;
+		final int FIRST_PRIME_NUMBERS_TO_PRINT = 1000;
+        final int ROWS_PER_PAGE = 50;
+        final int COLUMNS_PER_PAGE = 4;
         final int ORDMAX = 30;
-        int p [] = new int[M+1];
+        int primeNumbers [] = new int[FIRST_PRIME_NUMBERS_TO_PRINT+1];
         int pageNumber;
         int pageOffset;
         int rowoffset;
         int c;
-        int j;
-        int k;
-        boolean jprime;
+        int numberToBeChecked;
+        int nthPrimeNumber;
+//        boolean jprime;
         int ord;
         int square;
         int n = 0;
         int mult[] = new int[ORDMAX+1];
 
-        j=1;
-        k=1;
-        p[1] = 2;
+        numberToBeChecked = 1;
+        nthPrimeNumber=1;
+        primeNumbers[1] = 2;
         ord = 2;
         square = 9;
 
-        while (k < M) {
+        while (nthPrimeNumber < FIRST_PRIME_NUMBERS_TO_PRINT) {
+            boolean isNumberPrime;
             do {
-                j += 2;
-                if( j == square) {
+                numberToBeChecked += 2;
+                if( numberToBeChecked == square) {
                     ord++;
-                    square=p[ord]*p[ord];
-                    mult[ord-1]=j;
+                    square=primeNumbers[ord]*primeNumbers[ord];
+                    mult[ord-1]=numberToBeChecked;
                 }
                 n=2;
-                jprime=true;
-                while (n < ord && jprime) {
-                    while (mult[n]<j)
-                        mult[n] += p[n] + p[n];
-                    if (mult[n] == j)
-                        jprime=false;
+                isNumberPrime=true;
+                while (n < ord && isNumberPrime) {
+                    while (mult[n]<numberToBeChecked)
+                        mult[n] += primeNumbers[n] + primeNumbers[n];
+                    if (mult[n] == numberToBeChecked)
+                        isNumberPrime=false;
                     n++;
                 }
-            } while (!jprime);
-            k++;
-            p[k]=j;
+            } while (!isNumberPrime);
+            nthPrimeNumber++;
+            primeNumbers[nthPrimeNumber]=numberToBeChecked;
         }
         pageNumber = 1;
         pageOffset = 1;
-        while (pageOffset <= M) {
+        while (pageOffset <= FIRST_PRIME_NUMBERS_TO_PRINT) {
             System.out.print("The First ");
-            System.out.print(Integer.toString(M));
+            System.out.print(Integer.toString(FIRST_PRIME_NUMBERS_TO_PRINT));
             System.out.print(" Prime Numbers === Page ");
             System.out.print(Integer.toString(pageNumber));
             System.out.println("\n");
-            for (rowoffset= pageOffset; rowoffset <= pageOffset +RR-1; rowoffset++) {
-                for (c = 0; c <= CC - 1; c++)
-                    if (rowoffset + c * RR <= M)
-                        System.out.printf("%10d", p[rowoffset + c * RR]);
+            for (rowoffset= pageOffset; rowoffset <= pageOffset +ROWS_PER_PAGE-1; rowoffset++) {
+                for (c = 0; c <= COLUMNS_PER_PAGE - 1; c++)
+                    if (rowoffset + c * ROWS_PER_PAGE <= FIRST_PRIME_NUMBERS_TO_PRINT)
+                        System.out.printf("%10d", primeNumbers[rowoffset + c * ROWS_PER_PAGE]);
                 System.out.println();
             }
             System.out.println("\f");
             pageNumber++;
-            pageOffset += RR*CC;
+            pageOffset += ROWS_PER_PAGE*COLUMNS_PER_PAGE;
 
         }
 	}
