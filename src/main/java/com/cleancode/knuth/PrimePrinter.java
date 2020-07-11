b76c8d956a5e8ceb2e427d12249635704594aa56
Saad Ismail <saad.ismail@careem.com>
2020-07-11 16:19:22 +0500

    refactor into printer and calculator functions

--- src/main/java/com/cleancode/knuth/PrimePrinter.java
+++ src/main/java/com/cleancode/knuth/PrimePrinter.java
@@ -1,63 +1,84 @@
 package com.cleancode.knuth;
 
 public class PrimePrinter {
+
+    final int totalNumberOfPrimesToBePrinted = 1000;
+    int primes[] = new int[this.totalNumberOfPrimesToBePrinted +1];
+
     public static void main(String[] args) {
-		final int totalNumberOfPrimesToBePrinted = 1000;
-        final int RR = 50;
-        final int CC = 4;
-        final int ORDMAX = 30;
-        int primes[] = new int[totalNumberOfPrimesToBePrinted+1];
-        int PAGENUMBER;
-        int PAGEOFFSET;
-        int ROWOFFSET;
-        int C;
+
+        PrimePrinter primePrinter = new PrimePrinter();
+
+        primePrinter.calculatePrimes();
+        primePrinter.printPrimes();
+
+    }
+
+    private void calculatePrimes() {
+
         int currentNumberToCheck;
         int currentPrimeCount;
-        boolean JPRIME;
+
         int ORD;
         int SQUARE;
-        int N=0;
-        int MULT[] = new int[ORDMAX+1];
+        int N;
+
+        final int ORDMAX = 30;
+        int multiples[] = new int[ORDMAX+1];
 
         currentNumberToCheck=1;
         currentPrimeCount=1;
-        primes[1] = 2;
+
         ORD = 2;
         SQUARE = 9;
 
-        while (currentPrimeCount < totalNumberOfPrimesToBePrinted) {
+        primes[1] = 2;
+
+        while (currentPrimeCount < this.totalNumberOfPrimesToBePrinted) {
+            boolean isCurrentNumberPrime;
+
             do {
                 currentNumberToCheck += 2;
                 if( currentNumberToCheck == SQUARE) {
                     ORD++;
                     SQUARE=primes[ORD]*primes[ORD];
-                    MULT[ORD-1]=currentNumberToCheck;
+                    multiples[ORD-1]=currentNumberToCheck;
                 }
                 N=2;
-                JPRIME=true;
-                while (N < ORD && JPRIME) {
-                    while (MULT[N]<currentNumberToCheck)
-                        MULT[N] += primes[N] + primes[N];
-                    if (MULT[N] == currentNumberToCheck)
-                        JPRIME=false;
+                isCurrentNumberPrime=true;
+                while (N < ORD && isCurrentNumberPrime) {
+                    while (multiples[N]<currentNumberToCheck)
+                        multiples[N] += primes[N] + primes[N];
+                    if (multiples[N] == currentNumberToCheck)
+                        isCurrentNumberPrime=false;
                     N++;
                 }
-            }
-            while (!JPRIME);
+            } while (!isCurrentNumberPrime);
             currentPrimeCount++;
             primes[currentPrimeCount]=currentNumberToCheck;
         }
+    }
+
+    private void printPrimes() {
+        int PAGENUMBER;
+        int PAGEOFFSET;
+        int ROWOFFSET;
+
+        final int RR = 50;
+        final int CC = 4;
+
+        int C;
         PAGENUMBER = 1;
         PAGEOFFSET = 1;
-        while (PAGEOFFSET <= totalNumberOfPrimesToBePrinted) {
+        while (PAGEOFFSET <= this.totalNumberOfPrimesToBePrinted) {
             System.out.print("The First ");
-            System.out.print(Integer.toString(totalNumberOfPrimesToBePrinted));
+            System.out.print(Integer.toString(this.totalNumberOfPrimesToBePrinted));
             System.out.print(" Prime Numbers === Page ");
             System.out.print(Integer.toString(PAGENUMBER));
             System.out.println("\n");
             for (ROWOFFSET=PAGEOFFSET; ROWOFFSET <= PAGEOFFSET+RR-1; ROWOFFSET++) {
                 for (C = 0; C <= CC - 1; C++)
-                    if (ROWOFFSET + C * RR <= totalNumberOfPrimesToBePrinted)
+                    if (ROWOFFSET + C * RR <= this.totalNumberOfPrimesToBePrinted)
                         System.out.printf("%10d", primes[ROWOFFSET + C * RR]);
                 System.out.println();
             }
@@ -66,5 +87,5 @@ public class PrimePrinter {
             PAGEOFFSET += RR*CC;
 
         }
-	}
+    }
 }
